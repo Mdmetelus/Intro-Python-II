3b78e6f91dc2a4aaf7ce79c47663b128042aefc8
Mr. Metelus <Mdmetelus@gmail.com>
2019-03-20 18:30:29 -0400

    update...

--- src/room.py
+++ src/room.py
@@ -5,3 +5,9 @@ class Room():
     def __init__(self, name, description):
         self.name = name
         self.description = description
+        self.items= items
+        self.n_to = none
+        self.s_to = none
+        self.e_to = none
+        self.w_to = none
+    def get_room_in_direction(self, direction):
